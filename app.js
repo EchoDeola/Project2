969526adf520124dc5579f8263fbdf1d03d8d580
Ruth Adeola <71602285+EchoDeola@users.noreply.github.com>
2021-01-06 01:00:20 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+function check(){
+    document.getElementById('firstName').innerHTML = document.form.firstName.value;
+    document.getElementById('lastName').innerHTML = document.form.lastName.value;
+    document.getElementById('email').innerHTML = document.form.email.value;
+    ocument.getElementById('Gender').innerHTML = document.form.Gender.value;
+}
