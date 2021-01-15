cba120fb021373e73d90dcb41508f803f60a7724
Ruth Adeola <71602285+EchoDeola@users.noreply.github.com>
2021-01-15 03:49:16 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,38 @@
-function check(){
-    document.getElementById('firstName').innerHTML = document.form.firstName.value;
-    document.getElementById('lastName').innerHTML = document.form.lastName.value;
-    document.getElementById('email').innerHTML = document.form.email.value;
-    ocument.getElementById('Gender').innerHTML = document.form.Gender.value;
-}
+//const submit = document.getElementById("submit");
+//submit.addEventListener('click', displayDetails);
+
+let btnShw = document.querySelector("submit")
+btnShw.addEventListener("onclick", displayDetails)
+var row = 1;
+
+function displayDetails(){
+    var firstName = document.getElementById('firstName').value;
+    var lastName = document.getElementById('lastName').value;
+    var email = document.getElementById('email').value;
+    var Gender = document.getElementById('Gender').value;
+    var checkBox = check.addEventListener('check', getCheckboxStatus);
+        function getCheckboxStatus(){
+            var status = document.getElementById("checkBoxStatus").checked;
+            if (status) {
+            display("Yes");
+             } else {
+           display("No");
+            }
+         }
+   if(!firstName || !lastName || !email || !Gender || !checkBox){
+        alert("Please fill all the boxes");
+        return;
+        }   
+    }
+    var displayDetails = getElementById(display);
+        var newRow = displayDetails.insertRow(row);
+            var cel1 = NewRow.insertCell(0);
+			var cel2 = NewRow.insertCell(1);
+			var cel3 = NewRow.insertCell(2);
+            var cel4 = NewRow.insertCell(3);
+            var cel5 = NewRow.insertCell(4);
+            var cel6 = NewRow.insertCell(5);
+        row++;  
+        
+
+
